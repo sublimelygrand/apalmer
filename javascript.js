feeeaefdde10c99ad53b47ddfbde65c49a3c2beb
sublimelygrand <36170618+sublimelygrand@users.noreply.github.com>
2018-02-12 11:16:10 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function divideBy()
+        {
+        var miles = document.getElementById("miles").value;
+        var gallons = document.getElementById("gallons").value;
+        var mpg = document.getElementById("mpg").value = miles / gallons;
+        }
