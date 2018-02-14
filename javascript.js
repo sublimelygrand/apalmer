fd4a6d50c529fee188a6e90be37a6069c64624bf
sublimelygrand <36170618+sublimelygrand@users.noreply.github.com>
2018-02-13 22:22:08 -0800

    Delete javascript.js

--- javascript.js
+++
@@ -1,6 +0,0 @@
-function divideBy()
-        {
-        var miles = document.getElementById("miles").value;
-        var gallons = document.getElementById("gallons").value;
-        var mpg = document.getElementById("mpg").value = miles / gallons;
-        }
